7bfd57c4971a4bc020ec6593bf4b812931d58691
huseyinmertkok <huseyinmert.kok@gazi.edu.tr>
2021-08-17 11:05:28 +0300

    Fixing LogModel Issue

--- src/main/java/com/logs/logs/model/LogModel.java
+++ src/main/java/com/logs/logs/model/LogModel.java
@@ -1,5 +1,6 @@
 package com.logs.logs.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -8,19 +9,38 @@ import org.springframework.data.elasticsearch.annotations.Setting;
 
 import java.util.Date;
 
+@Data
 @Document(indexName="log_index")
 @Setting(settingPath = "static/es-settings.json")
 public class LogModel {
     @Id
     @Field(type = FieldType.Keyword)
     private String id;
+
+    @Field(type = FieldType.Object)
     private DeviceModel device;
+
+    @Field(type = FieldType.Text)
     private String name;
+
+    @Field(type = FieldType.Text)
     private LogLevelEnum level;
+
+    @Field(type = FieldType.Text)
     private String request;
+
+    @Field(type = FieldType.Text)
     private String response;
+
+    @Field(type = FieldType.Text)
     private String message;
+
+    @Field(type = FieldType.Date)
     private Date requestDateTime;
+
+    @Field(type = FieldType.Date)
     private Date responseDateTime;
+
+    @Field(type = FieldType.Date)
     private Date createdAt;
 }
